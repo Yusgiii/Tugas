7d624dfbac6783a86da27a123d069cd06dae5354
Yusgiii <114203370+Yusgiii@users.noreply.github.com>
2022-11-26 13:03:38 +0700

    Add files via upload

---
+++ tugas 7/satu/Outer.java
@@ -0,0 +1,21 @@
+package sesitujuh.satu;
+
+public class Outer {
+    int number = 0;
+    private class Inner{
+        public void print(){
+            System.out.println("Mengakses inner class yang ke: " + (++number));
+        }
+    }
+    void displayFromMethod(){
+        Inner in = new Inner();
+        in.print();
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Outer out = new Outer();
+        out.displayFromMethod();
+        out.displayFromMethod();
+    }
+}
